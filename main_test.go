9486e80d3cef1148f1b310a7e3028edf888951ee
emmanuelrdl <emmanuel.rudelle@hotmail.fr>
2017-06-23 14:27:09 +0000

    add addtional tests

--- main_test.go
+++ main_test.go
@@ -2,8 +2,13 @@ package main_test
 
 import (
     "os"
+    "strconv"
+    "encoding/json"
+    "bytes"
     "testing"
     "log"
+    "net/http"
+    "net/http/httptest"
     "."
 )
 
@@ -24,6 +29,103 @@ func TestMain(m *testing.M) {
 
     os.Exit(code)
 }
+
+
+
+func TestEmptyTable(t *testing.T) {
+    clearTable()
+
+    req, _ := http.NewRequest("GET", "/products", nil)
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusOK, response.Code)
+
+    if body := response.Body.String(); body != "[]" {
+        t.Errorf("Expected an empty array. Got %s", body)
+    }
+}
+
+
+func TestCreateProduct(t *testing.T) {
+    clearTable()
+
+    payload := []byte(`{"name":"test product","price":11.22}`)
+
+    req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(payload))
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusCreated, response.Code)
+
+    var m map[string]interface{}
+    json.Unmarshal(response.Body.Bytes(), &m)
+
+    if m["name"] != "test product" {
+        t.Errorf("Expected product name to be 'test product'. Got '%v'", m["name"])
+    }
+
+    if m["price"] != 11.22 {
+        t.Errorf("Expected product price to be '11.22'. Got '%v'", m["price"])
+    }
+
+    // the id is compared to 1.0 because JSON unmarshaling converts numbers to
+    // floats, when the target is a map[string]interface{}
+    if m["id"] != 1.0 {
+        t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+    }
+}
+
+func TestGetProduct(t *testing.T) {
+    clearTable()
+    addProducts(1)
+
+    req, _ := http.NewRequest("GET", "/product/1", nil)
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addProducts(count int) {
+    if count < 1 {
+        count = 1
+    }
+
+    for i := 0; i < count; i++ {
+        a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
+    }
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+    rr := httptest.NewRecorder()
+    a.Router.ServeHTTP(rr, req)
+
+    return rr
+}
+
+
+func TestGetNonExistentProduct(t *testing.T) {
+    clearTable()
+
+    req, _ := http.NewRequest("GET", "/product/11", nil)
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusNotFound, response.Code)
+
+    var m map[string]string
+    json.Unmarshal(response.Body.Bytes(), &m)
+    if m["error"] != "Product not found" {
+        t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got '%s'", m["error"])
+    }
+}
+
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+    if expected != actual {
+        t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+    }
+}
+
+
+
 
 func ensureTableExists() {
     if _, err := a.DB.Exec(tableCreationQuery); err != nil {
